b9450815a31d3200b20c129fd270f39c9c63f185
Pauline0306 <162077238+Pauline0306@users.noreply.github.com>
2024-10-10 04:23:30 +0800

    adding about details

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,39 @@
-<h1>about 'to beh</h1>
+<div>
+    <img src="pau.jpg" alt="pawwii" class="w-full h-auto max-w-sm rounded-lg mx-auto" />
+</div>
+<div class="w-full max-w-2xl">
+    <div class="flex flex-col gap-4 text-gray-300">
+        
+		<p>
+			Hi, I'm Pauline Astorga. I'm currently a 3rd-year college student pursuing a Bachelor of Science 
+            in Information Technology at Gordon College.
+			Information Technology wasn't my first course choice because my dream was to become a flight attendant, 
+            but for some reason, I ended up in technology. 
+			Now, my dream is to become a web developer and gain more knowledge about the tech industry. 
+			However, I'm not yet confident in my skills and know I still have a lot to learn in IT.
+			Being an IT student isn't easy, but nothing is too hard when you're willing to learn.
+		</p>
+
+		<p>
+			I'm a simple girl who doesn't know how to put on makeup or ride a bike, but I'm a little sporty. 
+            I know how to play basketball, bowling, and track and field.
+			I love watching sunsets, visiting the beach, and being in nature. 
+            I'm also interested in traveling to different places here in the Philippines.
+			I enjoy hanging out with my friends, spending time with them, and talking about life.
+		</p>
+
+		<p>
+		  I'm a simple girl who doesn't know how to put on makeup or ride a bike, but I'm a little sporty.
+          I know how to play basketball, bowling, and track and field.
+		  I love watching sunsets, visiting the beach, and being in nature. 
+          I'm also interested in traveling to different places here in the Philippines.
+		  I enjoy hanging out with my friends, spending time with them, and talking about life.
+		</p>
+
+		<p>I'm looking forward to the coming years, where I see myself becoming a web developer. 
+			This portfolio will be filled with posts of my achievements and successes in life. 
+            Nothing is impossible—have faith, keep trusting God, 
+			and believe that you can do it.
+		</p>
+	</div>
+</div>
